2a49cbb45753e2062675029f64eed6f0ef4ecd65
icyleaf <icyleaf.cn@gmail.com>
2017-07-20 19:12:15 +0800

    删除 xware，它的时代已经宣告结束 RIP

--- xware/monitor.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-while true; do
-    num=`ps | grep ETMDaemon | grep -v grep | grep -v check | wc -l`  # 计算进程数
-    if [ $num -lt 1 ]                                                 # 判断ETMDaemon进程是否存在
-        then ./portal                                                 # 重启进程的命令，请相应修改
-    fi
-    sleep 60
-done
