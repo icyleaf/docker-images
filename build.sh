15cc2f70b6dc723aa5b7502a54e5afa35d0d4f40
icyleaf <icyleaf.cn@gmail.com>
2018-10-29 11:21:13 +0800

    crystal: 增加 0.26.1 版本

--- build.sh
+++ build.sh
@@ -13,26 +13,24 @@ if [ ! -d $IMAGE_PATH ]; then
   exit 1
 fi
 
-if [ $IMAGE_PATH = "alpine" ]; then
-  ALPINE_VERSIONS=`ls $IMAGE_PATH`
-  LAST_VERSION=${ALPINE_VERSIONS[1]}
+if [ -z `ls ${IMAGE_PATH} | grep Dockerfile` ]; then
+  IMAGE_VERSIONS=`ls $IMAGE_PATH`
+  LATEST_VERSION=${IMAGE_VERSIONS[1]}
 
-  for ver in ${ALPINE_VERSIONS[@]}; do
+  for ver in ${IMAGE_VERSIONS[@]}; do
     if [ "$ver" != "README.md" ]; then
       echo
       echo "docker build $IMAGE_PATH:$ver -t icyleafcn/$IMAGE_PATH $OTHER_ARGS"
       echo
 
       docker build -t icyleafcn/$IMAGE_PATH:$ver "$IMAGE_PATH/$ver" $OTHER_ARG
-      if [ "$ver" = "$LAST_VERSION" ]; then
+      if [ "$ver" = "$LATEST_VERSION" ]; then
         echo "docker tag icyleafcn/$IMAGE_PATH:$ver icyleafcn/$IMAGE_PATH"
         docker tag icyleafcn/$IMAGE_PATH:$ver icyleafcn/$IMAGE_PATH
       fi
     fi
   done
-
 else
-
   echo
   echo "docker build $IMAGE_PATH -t icyleafcn/$IMAGE_PATH $OTHER_ARGS"
   echo
